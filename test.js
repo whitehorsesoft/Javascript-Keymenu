4ccaa47a80c6cde4ff9b9e954be7f155960cbfbb
devops <devops@whitehorsesoft.com>
2018-02-05 12:51:02 -0600

    base menu toggling structure in place

--- test.js
+++ test.js
@@ -1,24 +1,31 @@
-$('body').html('version 02');
+$('body').append('version 02');
 
 document.onkeyup = function(keyvalue) {
   var keyCode = keyvalue.keyCode;
   // left arrow
   if (keyCode == 37) {
-    var menu = GetMenu();
-    $('body').append(menu);
+    ShowMenu();
   }
-  else if (keyCode == 39) {
-    $('body').html('');
+  else {
+    $('#keymenu').hide();
   }
 }
 
-function Wrap(elementType, innerHtml) {
-  var result = '<' + elementType + '>';
+function Wrap(elementType, innerHtml, style = '') {
+  var result = '<' + elementType + style + '>';
   result += innerHtml;
   result += '</' + elementType + '>';
   return result;
 }
 
-function GetMenu() {
-  return Wrap('p', 'hello');
+function ShowMenu() {
+  var innerContentItems = [
+    Wrap('p', 'Item 1'),
+    Wrap('p', 'Item 2'),
+    Wrap('p', 'Item 3'),
+    Wrap('p', 'Item 4')
+  ];
+  var innerContent = innerContentItems.join('');
+  $('#keymenu').html(innerContent);
+  $('#keymenu').show();
 }
